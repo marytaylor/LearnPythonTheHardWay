e12629053f2d16a77d3d6ebc20117afdf667cf0e
marytaylor <marytaylor@weboniselab.com>
2015-12-12 20:10:48 -0500

    a drill from exercise 3

---
+++ ex3drill.py
@@ -0,0 +1,5 @@
+#How many days have I been alive today?
+# days in a year * my age + todays day (346)
+print (365 * 24) + 346
+
+#tested this with a calculator online. It says I have been born for 9049 days for Dec 12, 2015. I messed up because I didn't use my date of birth so 3 months were added amongst all of the years.
