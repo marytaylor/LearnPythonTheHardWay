d2f2688472f5cf78989a04430b8d3ab27f371d67
marytaylor <marytaylor@weboniselab.com>
2015-12-13 16:25:17 -0500

    Removed unnecessary comment

--- ex9.py
+++ ex9.py
@@ -12,5 +12,3 @@ With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """
-#Writing down mistakes: When trying to run this with python the file was saved in the wrong directory because I moved all exercises to a new directory.
-#Updated the file location, re-ran the file in python, and then submitting this to github.
