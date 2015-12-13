4187a600e47f6f90f030c25866a3a9bf694c083e
marytaylor <marytaylor@weboniselab.com>
2015-12-12 20:10:05 -0500

    Exercise 3 with all drills completed.  One drill was added as a seperate file so I will commit that file after this.

---
+++ ex3.py
@@ -0,0 +1,38 @@
+#this will print some text
+print " I will now count my chickens:"
+
+#this will print a word and output a number from calculations
+print "Hens", 25.0 + 30.0 / 6.0
+#this will print a word and output a number from calculations
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+#this will print some text
+print "Now I will count the eggs:"
+
+#this will output a number from calculations
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# this will print a sentence with numbers in it
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#this will output a true or false statement because there is a less than symbol
+print 3.0 + 2.0 < 5.0 - 7.0
+
+#this will print some text and a number
+print "What is 3 + 2?", 3.0 + 2.0
+#this will print some text and a number
+print "What is 5-7?", 5.0 - 7.0
+
+#this will output a sentence
+print "Oh, that's why it's False."
+
+#this will output a sentence
+print "How about some more."
+
+#this will output a sentence and a true/false output
+print "Is it greater?", 5.0 > -2.0
+
+#this will output a sentence and a true/false output
+print "Is it greater or equal?", 5.0 >= -2.0
+#this will output a sentence and a true/false output
+print "Is it less or equal?", 5.0 <= -2.0
