bbd45769af0d4e4dfb6964dd66280f410f24d44a
marytaylor <marytaylor@weboniselab.com>
2015-12-12 20:12:53 -0500

    Exercise 7 with all drills completed

---
+++ ex7.py
@@ -0,0 +1,27 @@
+#this prints a sentence
+print "Mary had a little lamb."
+#this prints a sentence with a string format being input
+print "Its fleece was white as %s." % 'snow'
+#this prints a sentence
+print "And everywhere that Mary went."
+#this prints a . 10 times
+print "." * 10 # what'd that do?
+
+#These all declare variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+#this prints all of the variables together.  The comma makes the next print line print on the same line as this one with a space between them.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
