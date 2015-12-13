3cab8546fef8c223a7462a987753c3ee029f2110
marytaylor <marytaylor@weboniselab.com>
2015-12-13 12:18:58 -0500

    Exercise 9 with all drills completed

---
+++ ex9.py
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+#Writing down mistakes: When trying to run this with python the file was saved in the wrong directory because I moved all exercises to a new directory.
+#Updated the file location, re-ran the file in python, and then submitting this to github.
