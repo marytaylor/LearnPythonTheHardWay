57d9ca5889b3221f2656c81e2b77384c3a6c1353
marytaylor <marytaylor@weboniselab.com>
2015-12-22 20:54:16 -0500

    Completed up to exercise 26.  Some python respositories were listed that I would like to read through more.

---
+++ ex22.py
@@ -0,0 +1,62 @@
+Make a list of every word and symbol that you have used.  Write what it does.
+
+#• python -> runs python scripts
+#• quit() -> quits python terminal when it is running.
+#• print -> prints any text within quotations.
+#• "" -> used to keep words together
+#• '' -> works the same as ""
+#• # -> octothorpe, pound, hash - used to create comments within code.
+#• + plus -> adds
+#• - minus -> subtracts or makes a negative
+#• / slash -> divides
+#• * asterisk -> multiplies
+#• % percent -> divides two values and outputs the remainder
+#• < less than
+#• > greater than
+#• <= less than or equal too
+#• <= greather than or equal too
+#• , -> used to separate arguments, makes sure an extra line is not added in
+#       some cases
+#• variables -> use this to declare a value, functions, or others things to a
+#       certain name
+#• _ -> used to represent spaces when creating variables, functions or other
+#       things
+#• % -> used to help with filling formatted characters
+#• %s -> this is the string formatted character
+#• %r -> also a string formatted character but this is used when the value needs
+#       to be 'read' also creates an object to help with debugging.
+#• %d -> integer formatting
+#• round() -> lets you round a floating point number
+#• string -> usually a bit of text but can contain format characters
+#• True -> a value or variable is True
+#• False -> a value or variable is False
+#• \n -> new line
+#• """ or ''' -> Lets you write muliple lines of text within one print function
+#• Escape Quotes: written as \' or \" within quotes to escape that specific quote
+#• \t -> creates a Tab
+#• \\ -> escapes \ so that \ will appear within text
+#• \b -> backspaces
+#• \r -> carriage return - like a new line
+#• raw_input() -> gets input from a person but removes any formatting (i.e.
+#       integers will be considered text.)
+#• from -> used to retrieve a module
+#• sys -> really useful module; it provides access to any command-line arguments
+#• import -> pulls in a specific method from the module
+#• argv -> argument vector
+#• open() -> opens a file
+#• read() -> reads a file
+#• close() -> closes and saves a file [depending on the way read is used closed
+#       is not necessary]
+#• truncate() -> This empties the file
+#• write() -> writes stuff inside of the file
+#• readline() -> reads just one line of the file
+#• os.path -> module; implements some useful funcitons on pathnames.
+#• exists -> function; returns TRUE if the path exists
+#• len() -> function that gives the length of a input
+#• def -> starts to define a function
+#• * -> tells python to take all the arguments and put them in args
+#• function -> does something
+#• variable -> gives a name to a value, function, or etc.
+#• seek() -> this deals with bytes and moves the files to the declared value.
+#• += -> increments a value
+#• return -> value from a function
---
+++ ex23.py
@@ -0,0 +1,13 @@
+Find some source code and read it.  The ready is to teach the following:
+
+1. Finding Python source code for things you need.
+2. Reading through the code and looking for files.
+3. Trying to understand code you find.
+
+Will need to look into these more later:
+https://github.com/clips/pattern
+https://github.com/scikit-learn/scikit-learn
+https://github.com/numenta/nupic
+https://github.com/matplotlib/matplotlib
+https://github.com/OpenExoplanetCatalogue/open_exoplanet_catalogue <- interesting data set
+https://github.com/divio/django-cms
