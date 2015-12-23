f612f213f19af3daca7f712879f96cdc20de7a22
marytaylor <marytaylor@weboniselab.com>
2015-12-22 21:52:31 -0500

    Exercise 28 completed.

---
+++ ex28.py
@@ -0,0 +1,45 @@
+# Exercise 28 write the below boolean practice problems and write what you think
+#   the answer will be.
+
+True and True
+# True
+False and True
+# False
+1 == 1 and 2 == 1
+# true and false = false
+"test" == "test"
+# true
+1 == 1 or 2 != 1
+# true or true = true
+True and 1 == 1
+# true and true = true
+False and 0 != 0
+# False and false = false
+True or 1 == 1
+# true or true = true
+"test" == "testing"
+# false
+1 != 0 and 2 == 1
+# true and false = false
+"test" != "testing"
+# true
+"test" == 1
+# false
+not (True and False)
+# not(False)= True
+not (1 == 1 and 0 != 1)
+# not(true and true)= false
+not (10 == 1 or 1000 == 1000)
+# not(false or true) = false
+not (1 != 10 or 3 == 4)
+# not(true or false) = false
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+# not (true and false) = true
+1 == 1 and (not ("testing" == 1 or 1 == 0))
+# true and not(false or false) = true and true = true
+"chucky" == "bacon" and (not (3 == 4 or 3 == 3))
+# false and not(false or true) = false and false = false
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+# true and not(true or false) = true and false = false
+
+# Yay! All were correct the first time.
