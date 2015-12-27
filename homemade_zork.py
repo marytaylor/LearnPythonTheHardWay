9565df13c2513b05984bdb50cec28bb9f02ab13e
marytaylor <marytaylor@weboniselab.com>
2015-12-27 10:44:26 -0500

    Exercise 33 without drills added.

---
+++ homemade_zork.py
@@ -0,0 +1,151 @@
+print """
+
+"""
+decision1 = raw_input("> ")
+
+if decision1 == "":
+    print ""
+
+    if decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+
+            if decision1 == "":
+                print ""
+            elif decision1 == "":
+                print ""
+            else decision1 == "":
+                print ""
+
+        elif decision1 == "":
+            print ""
+
+            if decision1 == "":
+                print ""
+            elif decision1 == "":
+                print ""
+            else decision1 == "":
+                print ""
+
+        else decision1 == "":
+            print ""
+
+            if decision1 == "":
+                print ""
+            elif decision1 == "":
+                print ""
+            else decision1 == "":
+                print ""
+
+    elif decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+
+            if decision1 == "":
+                print ""
+            elif decision1 == "":
+                print ""
+            else decision1 == "":
+                print ""
+
+        elif decision1 == "":
+            print ""
+
+            if decision1 == "":
+                print ""
+            elif decision1 == "":
+                print ""
+            else decision1 == "":
+                print ""
+
+        else decision1 == "":
+            print ""
+
+    else decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+
+            if decision1 == "":
+                print ""
+            elif decision1 == "":
+                print ""
+            else decision1 == "":
+                print ""
+
+        elif decision1 == "":
+            print ""
+
+            if decision1 == "":
+                print ""
+            elif decision1 == "":
+                print ""
+            else decision1 == "":
+                print ""
+
+        else decision1 == "":
+            print ""
+
+elif decision1 == "":
+    print ""
+
+    if decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+        elif decision1 == "":
+            print ""
+        else decision1 == "":
+            print ""
+
+    elif decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+        elif decision1 == "":
+            print ""
+        else decision1 == "":
+            print ""
+
+    else decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+        elif decision 1 == "":
+            print ""
+        else decision1 == "":
+            print ""
+
+else decision1 == "":
+    print ""
+
+    if decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+        elif decision1 == "":
+            print ""
+        else decision1 == "":
+            print ""
+
+    elif decision1 == "":
+        print ""
+
+        if decision1 == "":
+            print ""
+        elif decision1 == "":
+            print ""
+        else decision1 == "":
+            print ""
+
+    else decision1 == "":
+        print ""
